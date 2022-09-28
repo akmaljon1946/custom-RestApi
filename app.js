d64b2cccf8ae015f01d28466af7aa12f21c654ee
akmaljon <akmaljon.ahmedjanov@yandex.ru>
2022-09-28 10:53:33 +0500

    add

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.use(express.static(path.resolve(__dirname, 'client')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
 });
-app.listen(3000, () => console.log("Server has been started on port 3000..."));
+app.listen('resapi.netlify.app'() => console.log("Server has been started on port 3000..."));
